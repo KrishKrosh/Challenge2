0a3a7a0ae7a4e2f6bd4f89b74cac0a7aa88ef9d0
Krish Shah <KrishKrosh@users.noreply.github.com>
2020-07-01 19:30:34 -0600

    added login and signup

---
+++ src/components/Homepage.js
@@ -0,0 +1,61 @@
+import React from "react";
+import { Typography, Paper, Avatar, Button } from "@material-ui/core";
+import VerifiedUserOutlined from "@material-ui/icons/VerifiedUserOutlined";
+import withStyles from "@material-ui/core/styles/withStyles";
+import { Link } from "react-router-dom";
+import style from "./styles.css";
+
+const styles = (theme) => ({
+  main: {
+    width: "auto",
+    display: "block", // Fix IE 11 issue.
+  },
+  paper: {
+    display: "flex",
+
+    flex: 3,
+    flexDirection: "row",
+    height: 60,
+    padding: `${theme.spacing.unit * 1}px ${theme.spacing.unit * 3}px ${
+      theme.spacing.unit * 1
+    }px ${theme.spacing.unit * 3}px`,
+  },
+  avatar: {
+    backgroundColor: theme.palette.secondary.main,
+    margin: 2,
+    float: "right",
+  },
+  submit: {},
+});
+
+function HomePage(props) {
+  const { classes } = props;
+
+  return (
+    <main className={classes.main}>
+      <Paper elevation={0.5} className={classes.paper}>
+        <Typography component="h3" variant="h4">
+          YC Store
+        </Typography>
+
+        <Avatar className={classes.avatar}>
+          <Typography component="subtitle1" variant="subtitle2">
+            1000
+          </Typography>
+        </Avatar>
+        <Button
+          type="submit"
+          variant="contained"
+          color="secondary"
+          component={Link}
+          to="/login"
+          className={styles.submit}
+        >
+          Sign In
+        </Button>
+      </Paper>
+    </main>
+  );
+}
+
+export default withStyles(styles)(HomePage);
---
+++ src/firebase.js
@@ -0,0 +1,54 @@
+import app from "firebase/app";
+import "firebase/auth";
+import "firebase/firebase-firestore";
+
+const config = {
+  apiKey: " AIzaSyCqsl1lzlzJOShiCkqQU50ZHcBIpVcv2ds",
+  projectId: " ",
+};
+
+class Firebase {
+  constructor() {
+    app.initializeApp(config);
+    this.auth = app.auth();
+    this.db = app.firestore();
+  }
+
+  login(email, password) {
+    return this.auth.signInWithEmailAndPassword(email, password);
+  }
+
+  logout() {
+    return this.auth.signOut();
+  }
+
+  async register(name, email, password) {
+    await this.auth.createUserWithEmailAndPassword(email, password);
+    return this.auth.currentUser.updateProfile({
+      displayName: name,
+    });
+  }
+
+  isInitialized() {
+    return new Promise((resolve) => {
+      this.auth.onAuthStateChanged(resolve);
+    });
+  }
+
+  getCurrentUsername() {
+    return this.auth.currentUser && this.auth.currentUser.displayName;
+  }
+
+  getUid() {
+    console.log(this.auth.currentUser.uid);
+  }
+
+  async getCurrentUserQuote() {
+    const quote = await this.db
+      .doc(`users_codedamn_video/${this.auth.currentUser.uid}`)
+      .get();
+    return quote.get("quote");
+  }
+}
+
+export default new Firebase();

8324871124f557175cf416efd704528b49fb10d4
Krish Shah <KrishKrosh@users.noreply.github.com>
2020-07-06 09:03:47 -0600

    Added Promotions Carousel

---
+++ src/components/Promotions.js
@@ -0,0 +1,71 @@
+import React from "react";
+import { CircularProgress } from "@material-ui/core";
+import Carousel from "react-bootstrap/Carousel";
+import style from "./styles.css";
+
+class Promotions extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      promotions: {},
+      isLoading: true,
+    };
+  }
+  componentDidMount() {
+    this.getPromotions();
+  }
+  getPromotions() {
+    fetch("https://api.youthcomputing.ca/shop/promotions", {
+      method: "GET",
+      mode: "cors",
+    })
+      .then((response) => response.json())
+      // ...then we update the users state
+      .then((promotions) =>
+        this.setState({
+          promotions: promotions.promotions,
+          isLoading: false,
+        })
+      )
+      .catch((error) => {
+        console.error("There was an error!", error);
+      });
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        {this.state.isLoading ? (
+          <CircularProgress />
+        ) : (
+          <Carousel interval={3000} className="carouselWidth">
+            {this.shuffle(this.state.promotions).map((item, i) => (
+              <Carousel.Item key={i}>
+                <a href={this.state.promotions[i].event_url}>
+                  <img
+                    className={"d-block w-100 carouselImg "}
+                    src={this.state.promotions[i].image_url}
+                    alt="First slide"
+                  />
+                </a>
+              </Carousel.Item>
+            ))}
+          </Carousel>
+        )}
+      </React.Fragment>
+    );
+  }
+
+  shuffle(insertArray) {
+    let i = insertArray.length;
+    var array = insertArray;
+    while (i--) {
+      const ri = Math.floor(Math.random() * (i + 1));
+      [array[i], array[ri]] = [array[ri], array[i]];
+    }
+    return array;
+  }
+}
+
+export default Promotions;

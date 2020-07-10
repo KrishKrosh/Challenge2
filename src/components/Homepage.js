5765c279425f244035a9e5e8b7440f06bd9fc80c
Priyankdave14 <64758952+Priyankdave14@users.noreply.github.com>
2020-07-10 17:40:51 -0600

    store changed

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,12 +1,13 @@
 import React from "react";
 import NavBar from "./NavBar.js";
 import Promotions from "./Promotions.js";
-
+import MediaCard from "./Store.js"
 function HomePage(props) {
   return (
     <React.Fragment>
       <NavBar />
       <Promotions />
+      <MediaCard/>
     </React.Fragment>
   );
 }

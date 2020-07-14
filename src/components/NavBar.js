0f374f369ac096690db57f4988f8784053abd392
Krish Shah <KrishKrosh@users.noreply.github.com>
2020-07-13 19:59:02 -0600

    Navbar Style Changes

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,59 +1,44 @@
 import React from "react";
-import { useEffect, useState, useRef } from "react";
-import { Typography, Paper, Avatar, Button } from "@material-ui/core";
-import withStyles from "@material-ui/core/styles/withStyles";
+import {
+  Typography,
+  Paper,
+  Button,
+  TextField,
+  InputAdornment,
+} from "@material-ui/core";
+
+import SearchIcon from "@material-ui/icons/Search";
 import { Link } from "react-router-dom";
+import "./styles.css";
 
-const styles = (theme) => ({
-  main: {
-    width: "auto",
-    display: "block",
-  },
-  paper: {
-    display: "flex",
-    flex: 3,
-    flexDirection: "row",
-    height: 60,
-    padding: `${theme.spacing.unit * 1}px ${theme.spacing.unit * 3}px ${
-      theme.spacing.unit * 1
-    }px ${theme.spacing.unit * 3}px`,
-  },
-  avatar: {
-    backgroundColor: theme.palette.secondary.main,
-    margin: 2,
-    float: "right",
-  },
-  submit: {},
-});
-
-function NavBar(props) {
-  const { classes } = props;
+function NavBar() {
   return (
-    <main className={classes.main}>
-      <Paper elevation={0.5} className={classes.paper} position="sticky">
-        <Typography component="h3" variant="h4">
-          YC Store
-        </Typography>
-        <Avatar className={classes.avatar}>
-          <Typography component="subtitle1" variant="subtitle2">
-            1000
-          </Typography>
-        </Avatar>
-        <Button
-          type="submit"
-          variant="contained"
-          color="secondary"
-          component={Link}
-          to="/login"
-          className={styles.submit}
-        >
-          Sign In
-        </Button>
-
-        <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
-      </Paper>
-    </main>
+    <Paper elevation={0.5} className="navbarPaper">
+      <Typography component="h3" variant="h4">
+        YC Store
+      </Typography>
+      <TextField
+        className="navbarSearch"
+        InputProps={{
+          startAdornment: (
+            <InputAdornment position="start">
+              <SearchIcon />
+            </InputAdornment>
+          ),
+        }}
+      />
+      <Button
+        type="submit"
+        variant="contained"
+        color="secondary"
+        component={Link}
+        to="/login"
+        className="navbarItem"
+      >
+        Sign In
+      </Button>
+    </Paper>
   );
 }
 
-export default withStyles(styles)(NavBar);
+export default NavBar;

95394f31dfd0ed85290e3aedd4236d5ea5be62a8
Krish Shah <KrishKrosh@users.noreply.github.com>
2020-07-23 18:50:51 -0600

    added favicon and fixed footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,31 +4,16 @@ const footerStyle = {
   backgroundColor: "#F50057",
   fontSize: "20px",
   color: "white",
-  borderTop: "1px solid #E7E7E7",
   textAlign: "center",
-  padding: "20px",
-  left: "0",
-  bottom: "0",
-  height: "60px",
+  padding: "12px",
+  height: "50px",
   width: "100%",
 };
 
-const phantomStyle = {
-  display: "block",
-  padding: "20px",
-  height: "60px",
-  width: "100%",
-};
-
-function Footer({ children }) {
+function Footer() {
   return (
-    <div>
-      <div style={phantomStyle} />
-
-      <div style={footerStyle}>
-        {children}
-        <p>© YouthComputing 2020 </p>
-      </div>
+    <div style={footerStyle}>
+      <p>© YouthComputing 2020 </p>
     </div>
   );
 }

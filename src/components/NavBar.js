70c362a5f72e74e6bb19455c02e8152af695af54
Krish Shah <KrishKrosh@users.noreply.github.com>
2020-07-25 15:55:42 -0600

    fixed navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,4 @@
 import React from "react";
-
-import { Typography, Paper, Avatar, Button } from "@material-ui/core";
-import withStyles from "@material-ui/core/styles/withStyles";
-
 import {
   Typography,
   Paper,
@@ -14,47 +10,6 @@ import {
 import SearchIcon from "@material-ui/icons/Search";
 import { Link } from "react-router-dom";
 import "./styles.css";
-import SearchBar from 'material-ui-search-bar'
-render() ;
-  return(
-    <SearchBar
-      onChange={() => console.log('onChange')}
-      onRequestSearch={() => console.log('onRequestSearch')}
-      style={{
-        margin: '0 auto',
-        maxWidth: 800
-      }}
-    />
-  )
-  
-
-
-const styles = (theme) => ({
-  main: {
-    width: "auto",
-    display: "block", 
-  },
-  paper: {
-    display: "flex",
-    flex: 3,
-    flexDirection: "row",
-    height: 60,
-    padding: `${theme.spacing.unit * 1}px ${theme.spacing.unit * 3}px ${
-      theme.spacing.unit * 1
-    }px ${theme.spacing.unit * 3}px`,
-  },
-  avatar: {
-    backgroundColor: theme.palette.secondary.main,
-    margin: 2,
-    float: "right",
-  },
-  submit: {},
-});
-
-
-
-function NavBar(props) {
-  const { classes } = props;
 
 function NavBar() {
   return (
@@ -86,4 +41,4 @@ function NavBar() {
   );
 }
 
-export default NavBar}
+export default NavBar;

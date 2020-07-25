d9faad4b585a8b9e26560dcdd38127ebe36c4f20
Krish Shah <KrishKrosh@users.noreply.github.com>
2020-07-25 17:41:24 -0600

    category filtering

--- src/components/Store.js
+++ src/components/Store.js
@@ -1,14 +1,16 @@
 import React, { Component } from "react";
-import Categories from "./Categories.js";
 import MediaCard from "./Card.js";
-import { CircularProgress } from "@material-ui/core";
+import { CircularProgress, Button } from "@material-ui/core";
+import HorizontalScroller from "react-horizontal-scroll-container";
 class Store extends Component {
   constructor() {
     super();
 
     this.state = {
       cards: {},
+      categories: {},
       isLoading: true,
+      currentCategory: "",
     };
   }
 
@@ -25,6 +27,7 @@ class Store extends Component {
       .then((cards) =>
         this.setState({
           cards: cards.prizes,
+          categories: [...new Set(cards.prizes.map((x) => x.category))],
           isLoading: false,
         })
       )
@@ -40,15 +43,31 @@ class Store extends Component {
           <CircularProgress />
         ) : (
           <div>
-            <Categories />
-            <div className="store">
-              {this.state.cards.map((item, i) => (
-                <MediaCard
-                  points={item.points + " points"}
-                  name={item.name}
-                  image={item.image_url}
-                />
+            <HorizontalScroller>
+              {this.state.categories.map((item, i) => (
+                <Button
+                  key={i}
+                  variant="contained"
+                  color="secondary"
+                  className="categoryButton"
+                  onClick={() => this.setState({ currentCategory: item })}
+                >
+                  {item.replace("_", " ")}
+                </Button>
               ))}
+            </HorizontalScroller>
+            <div className="store">
+              {this.state.cards
+                .filter(
+                  (prize) => prize.category === this.state.currentCategory
+                )
+                .map((item, i) => (
+                  <MediaCard
+                    points={item.points + " points"}
+                    name={item.name}
+                    image={item.image_url}
+                  />
+                ))}
             </div>
           </div>
         )}

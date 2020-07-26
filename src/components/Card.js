84d6d3ce75e5b96aa8328bde07bcd7d4905acd82
LowCASHH <64927810+LowCASHH@users.noreply.github.com>
2020-07-25 21:15:15 -0600

    Link to login and redeem confirmation
    lol k

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,14 +5,34 @@ import CardContent from "@material-ui/core/CardContent";
 import CardMedia from "@material-ui/core/CardMedia";
 import { Typography } from "@material-ui/core";
 import "./styles.css";
+import Button from '@material-ui/core/Button';
+import Link from '@material-ui/core/Link';
+import { Link as RouterLink } from 'react-router-dom';
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import DialogTitle from "@material-ui/core/DialogTitle";
+
+
+//<Link component={RouterLink} to='/login'></Link>
+//</Link>
 
 class MediaCard extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      open: false
+    };
+  }
   render() {
     return (
-      <div>
+      <div> 
         <br />
+        
         <Card className="card" id={this.props.id}>
-          <CardActionArea className="cardHover">
+          <CardActionArea className="cardHover" onClick={() => this.setState({ open: true })}>
             <CardContent>
               <CardMedia component="img" image={this.props.image} />
               <br />
@@ -25,9 +45,38 @@ class MediaCard extends Component {
               <Typography variant="body1" component="h6" className="redeem">
                 Redeem Now
               </Typography>
+              
             </CardContent>
+            
           </CardActionArea>
         </Card>
+        {/* <Dialog
+          open={this.state.open}
+          onClose={() => this.setState({ open: false })}
+          aria-labelledby="alert-dialog-title"
+          aria-describedby="alert-dialog-description"
+        >
+          <DialogTitle id="alert-dialog-title">
+            {"Confirmation"}
+          </DialogTitle>
+          <DialogContent>
+            <DialogContentText id="alert-dialog-description">
+              Are you sure you want to redeem this prize?
+            </DialogContentText>
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={() => this.setState({ open: false })} color="primary">
+              No
+            </Button>
+            <Button
+              onClick={() => this.setState({ open: false })}
+              color="primary"
+              autoFocus
+            >
+              Yes
+            </Button>
+          </DialogActions>
+        </Dialog> */}
       </div>
     );
   }
